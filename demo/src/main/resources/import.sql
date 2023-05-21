0f16fdefd346ab7010f1c78fa37a262b0a9c0ac0
Manuel Villar <manuel.villar@usach.cl>
2023-05-21 16:44:57 -0400

    Parece que el commit anterior lo hice mal

---
+++ demo/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (01332, "Charles Aranguiz", 'B', "Si");
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (10101, "Carles Puyol", 'D', "Si");
