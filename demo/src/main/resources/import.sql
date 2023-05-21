c2b3bbe9edf1e13bf43d2ef869012c71cb19c3e8
Manuel Villar <manuel.villar@usach.cl>
2023-05-21 00:07:16 -0400

    Cambios para correr jenkins
    Se añaden los archivos de docker y jenkins

--- demo/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (01332, "Charles Aranguiz", 'B', "Si");
-INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (10101, "Carles Puyol", 'D', "Si");
-INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (10105, "Alexis Sanchez", 'A', "No");
