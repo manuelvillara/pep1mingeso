6a4f7034fecef722138dd6be536ff73818c591ef
Manuel Villar <manuel.villar@usach.cl>
2023-05-21 17:18:36 -0400

    probando

--- demo/src/main/resources/import.sql
+++ demo/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (01332, "Charles Aranguiz", 'B', "Si");
 INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (10101, "Carles Puyol", 'D', "Si");
+INSERT INTO proveedor(codigo,nombre,categoria,retencion) VALUES (12345, "Cristiano Ronaldo", 'A', "No");
